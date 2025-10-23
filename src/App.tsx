5b858b43c882d38f5cc0a772262d5ed45615dea5
senepote <senepote@gmail.com>
2025-10-23 16:23:46 -0700

    we got the bare bones people

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import Home from './pages/home/Home';
+import Designs from './pages/designs/Designs';
+import Resume from './pages/resume/Resume';
+import About from './pages/about/About';
+import Contact from './pages/contact/Contact';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Home />
+      <Designs />
+      <Resume />
+      <About />
+      <Contact />
+    </>
   );
 }
 
 export default App;
+
+
---
+++ src/pages/designs/Designs.tsx
@@ -0,0 +1,7 @@
+export default function Designs() {
+    return (
+        <div>
+            <h1>Designs</h1>
+        </div>
+    );
+}
